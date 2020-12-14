13839b00e3e4a9b4c10627c91cb37a1266ec55c5
Jordan <mulcah12@gmail.com>
2020-12-13 23:39:43 -0500

    refactor homepage to include image

--- jdesigns/src/components/home.js
+++ jdesigns/src/components/home.js
@@ -1,4 +1,5 @@
 import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 import './home.css'
 import JDesignsNavbar from './jDesignsNavbar'
 
@@ -8,11 +9,11 @@ function Home(){
             <header>
                 <JDesignsNavbar/>
             </header>
-            <h1>Need a beautiful website?</h1>
-            <h2>Go no further....</h2>
-            <div className="flex-container">
-                <Button>Schedule Consultation</Button>
-            </div>
+                <h1>Need a beautiful website?</h1>
+                <h2>Go no further....</h2>
+                <div className="flex-container">
+                    <Button>Schedule Consultation</Button>
+                </div>
         </div>
     );
 }
